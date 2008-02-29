a7fd64d6218ae178a48b0c0050a131a9e170eeb3
tvburger <tvburger@9cac385e-2f45-0410-8df2-cbf9a85a4dd8>
2008-02-29 20:39:34 +0000

    NetBeans project for the roomware server library files

---
+++ NetBeansProjects/RoomWare Server/src/org/roomwareproject/server/DeviceAddress.java
@@ -0,0 +1,11 @@
+package org.roomwareproject.server;
+
+public abstract class DeviceAddress {
+
+	public abstract String toString();
+
+	public abstract boolean equals(Object obj);
+
+	public abstract int hashCode();
+
+}
