679530a5e89d93275eac3c693d2413a8e5129838
tvburger <tvburger@9cac385e-2f45-0410-8df2-cbf9a85a4dd8>
2008-10-03 14:45:15 +0000

    cleanup

--- trunk/Server/program/bin/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set classpath=""
-
-for %%f in ( lib\*.jar ) do @call bin\setclasspath %%f
-
-
-java -cp %classpath% -DRoomWareConfig="conf/roomware.conf" org.roomwareproject.server.impl.RoomWareServerImpl
