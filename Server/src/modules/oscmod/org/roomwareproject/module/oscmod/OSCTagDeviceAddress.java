9d0679f0dd2ec4d15cc2a0bd87a179d290effa82
tvburger <tvburger@9cac385e-2f45-0410-8df2-cbf9a85a4dd8>
2009-06-24 08:33:13 +0000

    osc module added to svn, little bugfix in Presence

---
+++ Server/src/modules/oscmod/org/roomwareproject/module/oscmod/OSCTagDeviceAddress.java
@@ -0,0 +1,32 @@
+package org.roomwareproject.module.oscmod;
+
+import org.roomwareproject.server.*;
+
+public class OSCTagDeviceAddress extends DeviceAddress {
+	protected String address;
+	protected String type = "OSCTag";
+
+	public OSCTagDeviceAddress(Object arg1, Object arg2) {
+		address = arg1 + "-" + arg2;
+	}
+
+	public int hashCode() {
+		return address.hashCode();
+	}
+
+	public boolean equals(Object object) {
+		if(object instanceof OSCTagDeviceAddress) {
+			OSCTagDeviceAddress other = (OSCTagDeviceAddress) object;
+			return address.equals(other.address);
+		}
+		return false;
+	}
+
+	public String toString() {
+		return address;
+	}
+
+	public String getType () {
+		return type;
+	}
+}
